25145f007303dc6284943c1c206f39cf34d1e2f1
Ali Almasli <ali.almasli.98@gmail.com>
2026-07-05 00:15:39 +0400

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -4,20 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "impulse-ios-sdk",
+    name: "ImpulseSDK",
+    platforms: [
+        .iOS(.v15),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "impulse-ios-sdk",
-            targets: ["impulse-ios-sdk"]
+            name: "ImpulseSDK",
+            targets: ["ImpulseSDK"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "impulse-ios-sdk"
-        ),
-
+        .target(name: "ImpulseSDK", path: "Sources/ImpulseSDK"),
     ]
 )
